11b5d95b2963b2b652bf8dc1846b53a55e121a28
shahu_pawar <shahu043@gmail.com>
2023-03-09 17:31:14 +0000

    working code

--- example.tf
+++ example.tf
@@ -1,56 +1,58 @@
 provider "aws" {
-  region     = "ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "web" {
   ami           = "ami-09ba48996007c8b50"
   instance_type = "t2.micro"
-  key_name = aws_key_pair.terraform-19.key_name
+  availability_zone = "ap-south-1a"
+  key_name      = aws_key_pair.terraform-19.key_name
 
   tags = {
     Name = "terraform-19"
   }
+/*
   provisioner "local-exec" {
-command = "echo ${aws_instance.web.private_ip} > ip_address.txt"
-}
-provisioner "remote-exec" {
-inline = [
-"sudo amazon-linux-extras enable nginx1.12",
-"sudo yum -y install nginx",
-"sudo systemctl start nginx",
-]
-}
-
-  depends_on = [aws_ebs_volume.terraform-19]
+    command = "echo ${aws_instance.web.private_ip} > ip_address.txt"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras enable nginx1.12",
+      "sudo yum -y install nginx",
+      "sudo systemctl start nginx",
+    ]
+  }
+*/
+  depends_on      = [aws_ebs_volume.terraform-19]
   security_groups = ["${aws_security_group.AWSaccess.name}"]
 }
 
 resource "aws_ebs_volume" "terraform-19" {
-availability_zone = "ap-south-1a"
-size = 10
+  availability_zone = "ap-south-1a"
+  size              = 10
 
- tags = {
+  tags = {
     Name = "terraform-19"
   }
 }
 resource "aws_volume_attachment" "ebs_add" {
-device_name = "/dev/xvdf"
-volume_id = aws_ebs_volume.terraform-19.id
-instance_id = aws_instance.web.id
+  device_name = "/dev/xvdf"
+  volume_id   = aws_ebs_volume.terraform-19.id
+  instance_id = aws_instance.web.id
 }
 
 resource "aws_eip" "terraform-19" {
-instance = aws_instance.web.id
-vpc = true
+  instance = aws_instance.web.id
+  vpc      = true
 
- tags = {
+  tags = {
     Name = "terraform-19"
   }
 }
 
 
 output "ip" {
-value = aws_instance.web.public_ip
+  value = aws_instance.web.public_ip
 }
 
 
--- key.tf
+++ key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "terraform-19" {
-key_name = "terraform-19"
-public_key = "${file("C:\\Users\\sfjbs\\.ssh\\id_rsa.pub")}"
+  key_name   = "terraform-19"
+  public_key = file("id_rsa.pub")
 }
